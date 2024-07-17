e66ba97c808d8ceaac4a98df6e25b0128551fa5f
sh-oon <jshyh1@naver.com>
2024-07-18 00:46:17 +0900

    feat / eslint 추가

--- src/app/(tabs)/(settings)/index.tsx
+++ src/app/(tabs)/(settings)/index.tsx
@@ -1,112 +1,110 @@
-import {StyleSheet, SafeAreaView, Animated} from 'react-native';
+import { StyleSheet, SafeAreaView, Animated } from "react-native";
 
-import {ThemedText} from '@/components/atoms/ThemedText';
+import { ThemedText } from "@/components/atoms/ThemedText";
 import ListItem from "@/components/molecules/ListItem";
-import {router} from "expo-router";
+import { router } from "expo-router";
 import SectionList = Animated.SectionList;
-import {useColorScheme, useColorTheme} from "@/hooks/useColorScheme";
-import {Colors} from "@/constants/Colors";
-import * as Application from 'expo-application';
-import {useTheme} from "@/context/ThemeContext";
-
+import { Colors } from "@/constants/Colors";
+import * as Application from "expo-application";
+import { useTheme } from "@/context/ThemeContext";
 
 export type TMenuItem = {
-  title: string;
-  type: 'switch' | 'button' | 'link' | 'label';
-  label?: string
-  onPress?: () => void;
-}
+	title: string;
+	type: "switch" | "button" | "link" | "label";
+	label?: string;
+	onPress?: () => void;
+};
 
 export type TSettingItems = {
-  title: string;
-  data: TMenuItem[];
-}
+	title: string;
+	data: TMenuItem[];
+};
 
 export default function SettingScreen() {
-  const { theme } = useTheme()
-  const colors = Colors[theme ?? 'light']
+	const { theme } = useTheme();
 
+	const settings: TSettingItems[] = [
+		{
+			title: "설정",
+			data: [
+				{
+					title: "언어",
+					type: "link",
+					onPress: () => router.push("/(settings)/language"),
+				},
+				{
+					title: "테마",
+					type: "link",
+					onPress: () => router.push("/(settings)/theme"),
+				},
+				{
+					title: "알람",
+					type: "link",
+					onPress: () => console.log("Notification"),
+				},
+			],
+		},
+		{
+			title: "앱 정보 및 이용약관",
+			data: [
+				{
+					title: "앱 버전",
+					type: "label",
+					label: `v${Application.nativeApplicationVersion}` ?? "",
+				},
+				{
+					title: "Terms of Service",
+					type: "link",
+					onPress: () => console.log("Terms of Service"),
+				},
+				{
+					title: "Contact Us",
+					type: "link",
+					onPress: () => console.log("Contact Us"),
+				},
+			],
+		},
+	];
 
-  const settings: TSettingItems[] = [
-    {
-      title: '설정',
-      data: [
-        {
-          title: '언어',
-          type: 'link',
-          onPress: () => router.push('/(settings)/language'),
-        },
-        {
-          title: '테마',
-          type: 'link',
-          onPress: () => router.push('/(settings)/theme'),
-        },
-        {
-          title: '알람',
-          type: 'link',
-          onPress: () => console.log('Notification'),
-        },
-      ],
-    },
-    {
-      title: '앱 정보 및 이용약관',
-      data: [
-        {
-          title: '앱 버전',
-          type: 'label',
-          label: `v${Application.nativeApplicationVersion}` ?? ''
-        },
-        {
-          title: 'Terms of Service',
-          type: 'link',
-          onPress: () => console.log('Terms of Service'),
-        },
-        {
-          title: 'Contact Us',
-          type: 'link',
-          onPress: () => console.log('Contact Us'),
-        },
-      ],
-    },
-  ]
-
-  return (
-    <SafeAreaView
-      style={{
-        backgroundColor: theme === 'light' ? Colors.light.space : Colors.dark.space,
-        paddingTop: 10
-      }}
-    >
-      <SectionList
-        sections={settings}
-        keyExtractor={(item, index) => item.title + index}
-        style={{
-          height: '100%'
-        }}
-        renderSectionHeader={({section: {title}}) => (
-          <ThemedText
-            type={'defaultSemiBold'}
-            style={styles.stepHeader}
-          >{title}</ThemedText>
-        )}
-        renderItem={({item}) => (
-          <ListItem
-            title={item.title}
-            type={item.type}
-            onPress={item.onPress}
-            label={item.label}
-          />
-        )}
-      />
-    </SafeAreaView>
-  );
+	return (
+		<SafeAreaView
+			style={{
+				backgroundColor: theme === "light" ? Colors.light.space : Colors.dark.space,
+				paddingTop: 10,
+			}}
+		>
+			<SectionList
+				sections={settings}
+				keyExtractor={(item, index) => item.title + index}
+				style={{
+					height: "100%",
+				}}
+				renderSectionHeader={({ section: { title } }) => (
+					<ThemedText
+						type={"defaultSemiBold"}
+						style={styles.stepHeader}
+					>
+						{title}
+					</ThemedText>
+				)}
+				renderItem={({ item }) => (
+					<ListItem
+						title={item.title}
+						type={item.type}
+						onPress={item.onPress}
+						label={item.label}
+					/>
+				)}
+			/>
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  settingContainer: {
-    paddingBottom: 10
-  },
-  stepHeader: {
-    padding: 10,
-  },
+	settingContainer: {
+		paddingBottom: 10,
+	},
+	stepHeader: {
+		padding: 10,
+	},
 });
--- src/hooks/useColorScheme.tsx
+++ src/hooks/useColorScheme.tsx
@@ -1,27 +1,27 @@
 import useAsyncStorage from "@/lib/useAsyncStorage";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
-import {ColorSchemeName, useColorScheme} from 'react-native';
-export {useColorScheme}
+import { ColorSchemeName, useColorScheme } from "react-native";
+export { useColorScheme };
 
-export type TAppTheme = 'light' | 'dark' | 'system';
+export type TAppTheme = "light" | "dark" | "system";
 
 export const useColorTheme = () => {
-  const [storedTheme, setStoredTheme, resetStoredTheme] = useAsyncStorage<TAppTheme>('@user_theme', 'light');
-  const [theme, setTheme] = useState<ColorSchemeName>('light');
-  const systemTheme = useColorScheme();
+	const [storedTheme, setStoredTheme, resetStoredTheme] = useAsyncStorage<TAppTheme>("@user_theme", "light");
+	const [theme, setTheme] = useState<ColorSchemeName>("light");
+	const systemTheme = useColorScheme();
 
-  useEffect(() => {
-    if (storedTheme === 'system') {
-      setTheme(systemTheme);
-    } else {
-      setTheme(storedTheme);
-    }
-  }, [storedTheme]);
+	useEffect(() => {
+		if (storedTheme === "system") {
+			setTheme(systemTheme);
+		} else {
+			setTheme(storedTheme);
+		}
+	}, [storedTheme]);
 
-  useEffect(() => {
-    console.log('theme', theme)
-  }, [theme]);
+	useEffect(() => {
+		console.log("theme", theme);
+	}, [theme]);
 
-  return {storedTheme, setStoredTheme, resetStoredTheme, state: theme};
-}
+	return { storedTheme, setStoredTheme, resetStoredTheme, state: theme };
+};
