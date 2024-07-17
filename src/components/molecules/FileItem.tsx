179f242b7332d716acc11540864947a05b8e519f
sh-oon <jshyh1@naver.com>
2024-07-18 00:49:01 +0900

    chore / lint 적용

--- src/components/molecules/FileItem.tsx
+++ src/components/molecules/FileItem.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Dimensions, StyleSheet, TouchableHighlight, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { ThemedView } from "../atoms/ThemedView";
 import { ThemedIcon } from "../atoms/ThemedIcon";
 import { FileInfo } from "expo-file-system";
@@ -8,68 +8,65 @@ import { ThemedText } from "../atoms/ThemedText";
 import { convertDataCapacity } from "@/utils/common";
 
 type TFileItemProps = {
-  name: string;
-  extention: "pdf" | "txt";
-  fileInfo: FileInfo;
-  width?: number;
+	name: string;
+	extention: "pdf" | "txt";
+	fileInfo: FileInfo;
+	width?: number;
 };
 
-const FileItem = ({
-  name,
-  extention,
-  fileInfo,
-  width = 0,
-  ...rest
-}: TFileItemProps) => {
-  const color = useThemeColor({}, "text");
+const FileItem = ({ name, extention, fileInfo, width = 0, ...rest }: TFileItemProps) => {
+	const color = useThemeColor({}, "text");
 
-  return (
-    <ThemedView style={[styles.container, { borderColor: color, width }]}>
-      <ThemedIcon extention={extention} />
-      <ThemedView style={{ backgroundColor: "transparent" }}>
-        <ThemedText
-          type="subtitle"
-          style={styles.name}
-          numberOfLines={1}
-          ellipsizeMode="tail"
-        >
-          {name}
-        </ThemedText>
-        {fileInfo.exists && (
-          <ThemedText lightColor="#808080" style={styles.size}>
-            {convertDataCapacity(fileInfo.size)}
-          </ThemedText>
-        )}
-      </ThemedView>
-    </ThemedView>
-  );
+	return (
+		<ThemedView style={[styles.container, { borderColor: color, width }]}>
+			<ThemedIcon extention={extention} />
+			<ThemedView style={{ backgroundColor: "transparent" }}>
+				<ThemedText
+					type="subtitle"
+					style={styles.name}
+					numberOfLines={1}
+					ellipsizeMode="tail"
+				>
+					{name}
+				</ThemedText>
+				{fileInfo.exists && (
+					<ThemedText
+						lightColor="#808080"
+						style={styles.size}
+					>
+						{convertDataCapacity(fileInfo.size)}
+					</ThemedText>
+				)}
+			</ThemedView>
+		</ThemedView>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "space-between",
-    paddingVertical: 16,
-    paddingHorizontal: 8,
-    gap: 24,
-    borderRadius: 8,
-    backgroundColor: "rgba(255, 255, 255, 0.1)",
-  },
-  icon: {
-    marginRight: 16,
-  },
-  name: {
-    textAlign: "center",
-    fontSize: 14,
-    // 줄넘김 없애기
-    overflow: "hidden",
-  },
-  size: {
-    textAlign: "center",
-    fontSize: 10,
-  },
+	container: {
+		flex: 1,
+		flexDirection: "column",
+		alignItems: "center",
+		justifyContent: "space-between",
+		paddingVertical: 16,
+		paddingHorizontal: 8,
+		gap: 24,
+		borderRadius: 8,
+		backgroundColor: "rgba(255, 255, 255, 0.1)",
+	},
+	icon: {
+		marginRight: 16,
+	},
+	name: {
+		textAlign: "center",
+		fontSize: 14,
+		// 줄넘김 없애기
+		overflow: "hidden",
+	},
+	size: {
+		textAlign: "center",
+		fontSize: 10,
+	},
 });
 
 export default FileItem;
